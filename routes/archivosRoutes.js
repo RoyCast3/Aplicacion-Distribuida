69bf8869648953e792cdb1e8577279203184732d
F3rshy2111 <116702851+F3rshy2111@users.noreply.github.com>
2024-01-09 23:06:38 -0600

    Comentarios

--- routes/archivosRoutes.js
+++ routes/archivosRoutes.js
@@ -7,11 +7,13 @@ const archivosController = require('../controllers/archivosController');
 //Creamos el Router en express
 const router = express.Router();
 
-//Endpoint para subir archivos desde un 
+//Endpoint para subir archivos en un POST
 router.post("/upload", archivosController.uploadFile, archivosController.confirmacion);
 
+//Endpoint para obtener la lista de archivos que se encuentran en la carpeta uploads
 router.get("/files", archivosController.getFiles);
 
+///Endpoint para descargar archivos especificos.
 router.get("/download/:filename", archivosController.downloadFile);
 
 module.exports = router;
